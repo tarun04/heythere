89a40bd08f335c1c57dd13136a3bb1cbb9e08d96
Tarun Reddy <tarunreddy.04@outlook.com>
2020-12-03 22:56:31 -0600

    Basic project layout setup

---
+++ heythere/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using heythere.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace heythere.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ heythere/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using heythere.Models;
+using heythere.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace heythere.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
